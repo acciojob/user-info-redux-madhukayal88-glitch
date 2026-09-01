0e8f8e83bfdd5ff8b4a136bc899ad720a6eab38f
kayalvizhi <madhukayal88@gmail.com>
2026-09-01 18:03:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { updateName, updateEmail } from './redux/userSlice';
-import './App.css';
+import UserInfo from './components/UserInfo';
+import './App.css'; // optional
 
 function App() {
-  const dispatch = useDispatch();
-  const { name, email } = useSelector((state) => state.user);
-
-  const handleNameChange = (e) => {
-    dispatch(updateName(e.target.value));
-  };
-
-  const handleEmailChange = (e) => {
-    dispatch(updateEmail(e.target.value));
-  };
-
   return (
     <div className="App">
-      <h1>User Information</h1>
-      
-      <div className="form">
-        <input
-          type="text"
-          placeholder="Enter your name"
-          value={name}
-          onChange={handleNameChange}
-        />
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-      </div>
-
-      <div className="output">
-        <p>Name - {name || 'No name entered'}</p>
-        <p>Email - {email || 'No email entered'}</p>
-      </div>
+      <UserInfo />
     </div>
   );
 }
