ce4389874cbc107c8cf04172f2553a4bb7235d2e
madhukayal88-glitch <madhukayal88@gmail.com>
2026-09-09 12:33:33 +0530

    Update form structure and state selectors

--- src/App.js
+++ src/App.js
@@ -3,26 +3,35 @@ import { useSelector, useDispatch } from 'react-redux';
 import { updateName, updateEmail } from './redux/userSlice';
 
 function App() {
-  const { name, email } = useSelector((state) => state.user);
+  const name = useSelector((state) => state.user.name);
+  const email = useSelector((state) => state.user.email);
   const dispatch = useDispatch();
 
   return (
     <div>
       <h1>User Information</h1>
-      <div>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => dispatch(updateName(e.target.value))}
-          placeholder="Enter Name"
-        />
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => dispatch(updateEmail(e.target.value))}
-          placeholder="Enter Email"
-        />
-      </div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div>
+          <label htmlFor="name">Name: </label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => dispatch(updateName(e.target.value))}
+            placeholder="Enter Name"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email: </label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => dispatch(updateEmail(e.target.value))}
+            placeholder="Enter Email"
+          />
+        </div>
+      </form>
       <div className="output">
         <p>Name - {name}</p>
         <p>Email - {email}</p>
