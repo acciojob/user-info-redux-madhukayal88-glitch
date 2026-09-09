b81824a9efe2eafe07264ba4727584a098e1af84
madhukayal88-glitch <madhukayal88@gmail.com>
2026-09-09 12:32:11 +0530

    Fix package.json and complete Redux User Info implementation

---
+++ src/App.js
@@ -0,0 +1,34 @@
+﻿import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateName, updateEmail } from './redux/userSlice';
+
+function App() {
+  const { name, email } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>User Information</h1>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => dispatch(updateName(e.target.value))}
+          placeholder="Enter Name"
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => dispatch(updateEmail(e.target.value))}
+          placeholder="Enter Email"
+        />
+      </div>
+      <div className="output">
+        <p>Name - {name}</p>
+        <p>Email - {email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/userSlice.js
@@ -0,0 +1,20 @@
+﻿import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    name: '',
+    email: '',
+  },
+  reducers: {
+    updateName: (state, action) => {
+      state.name = action.payload;
+    },
+    updateEmail: (state, action) => {
+      state.email = action.payload;
+    },
+  },
+});
+
+export const { updateName, updateEmail } = userSlice.actions;
+export default userSlice.reducer;
