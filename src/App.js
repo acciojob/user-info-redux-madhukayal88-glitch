6363e7d750566fd836215f3e02b01f28246a3587
kayalvizhi <madhukayal88@gmail.com>
2026-09-11 07:47:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-﻿import React from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { updateName, updateEmail } from './redux/userSlice';
+import { updateName, updateEmail } from './redux/userActions';
 
 function App() {
-  const name = useSelector((state) => state.user.name);
-  const email = useSelector((state) => state.user.email);
   const dispatch = useDispatch();
+  const name = useSelector((state) => state.name);
+  const email = useSelector((state) => state.email);
+
+  const handleNameChange = (e) => {
+    dispatch(updateName(e.target.value));
+  };
+
+  const handleEmailChange = (e) => {
+    dispatch(updateEmail(e.target.value));
+  };
 
   return (
     <div>
       <h1>User Information</h1>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => dispatch(updateName(e.target.value))}
-      />
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => dispatch(updateEmail(e.target.value))}
-      />
+      <form>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+            placeholder="Enter your name"
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Enter your email"
+          />
+        </label>
+      </form>
+
       <div className="output">
-        <div>Name - {name}</div>
-        <div>Email - {email}</div>
+        <p>Name - {name}</p>
+        <p>Email - {email}</p>
       </div>
     </div>
   );
