7160c3fc44095a7e75ac45a649f81f55bf233808
kayalvizhi <madhukayal88@gmail.com>
2026-09-16 07:28:22 +0530

    update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
-﻿import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './userSlice';
+import { createStore } from 'redux';
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
+const initialState = { name: '', email: '' };
+
+function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'SET_NAME': return { ...state, name: action.payload };
+    case 'SET_EMAIL': return { ...state, email: action.payload };
+    default: return state;
+  }
+}
+
+export const store = createStore(userReducer);
