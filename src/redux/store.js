9ac0076c64cd1f0a12f3b243e02dcf2efbfe9a85
kayalvizhi <madhukayal88@gmail.com>
2026-09-24 12:59:30 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,10 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import userReducer from "./user/userReducer";
+import { createStore, combineReducers } from "redux";
+import userReducer from "./reducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer);
 
 export default store;
