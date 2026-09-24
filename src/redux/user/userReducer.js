eb1aa2bddf56d3c0821ee92d1349edd4def7f4f3
kayalvizhi <madhukayal88@gmail.com>
2026-09-24 11:15:35 +0530

    Create userReducer.js

---
+++ src/redux/user/userReducer.js
@@ -0,0 +1,26 @@
+import {
+  FETCH_USER_REQUEST,
+  FETCH_USER_SUCCESS,
+  FETCH_USER_FAILURE,
+} from "./userTypes";
+
+const initialState = {
+  loading: false,
+  user: null,
+  error: null,
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USER_REQUEST:
+      return { ...state, loading: true, error: null };
+    case FETCH_USER_SUCCESS:
+      return { ...state, loading: false, user: action.payload, error: null };
+    case FETCH_USER_FAILURE:
+      return { ...state, loading: false, user: null, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
